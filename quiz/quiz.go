c2808c1ca353931a13a53484b4e10e029cf7991c
Kathleen West <2509480w@student.gla.ac.uk>
2022-08-30 22:18:35 +0100

    Project Development
    Changes:
    - adding comments to outline the main flow of the application

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -6,4 +6,11 @@ import (
 
 func main() {
 	fmt.Println("Quiz Program")
+	// scanner input?
+	// parse and start timer
+	// loop until time runs out
+	// print question
+	// monitor until newline input
+	// repeat
+	// display results?
 }
